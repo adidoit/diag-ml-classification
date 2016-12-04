e7bdecea070112911dd53f5117daf66e7fcacea1
adi pradhan <adip@umich.edu>
2016-12-04 15:42:28 -0500

    refactor performance measures into helper functions

--- diag-ml.R
+++ diag-ml.R
@@ -22,6 +22,24 @@ library(dplyr)
 library(ROCR)
 library(e1071)
 
+# helper function------
+perf_measures <- function(actual,predicted){
+  c_matrix <- table(actual,predicted)
+  print(c_matrix)
+  print("accuracy:")
+  accuracy <- (c_matrix[1,1] + c_matrix[2,2])/(c_matrix[1,1] + c_matrix[2,2]+c_matrix[1,2] + c_matrix[2,1])
+  print(accuracy*100)
+  print("sensitivity (tpr):")
+  sensitivity <- c_matrix[2,2]/(c_matrix[2,2]+c_matrix[2,1])
+  print(sensitivity)
+  print("specificity (1-fpr):")
+  fpr <- c_matrix[1,2]/(c_matrix[1,1]+c_matrix[1,2])
+  specificity <- 1 - fpr
+  print(specificity*100)
+  cat("\n\n")
+}
+# end helper function -----
+
 data <- read.csv('data/labelled_data.csv')
 unlabelled <- read.csv('data/unlabelled_data.csv')
 
@@ -54,22 +72,23 @@ auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
 print("#Logistic Regression")
 print(auc)
+cat("\n")
 
 # Decision Tree Model
 rpart_model <- rpart(Survived ~ .,data=train, method='class')
 rpart.plot(rpart_model)
 rpart_pred <- predict(rpart_model,newdata = test,type = 'class')
 print("#Classification and Regression Trees (CART)")
-print(table(test$Survived,rpart_pred))
+perf_measures(test$Survived,rpart_pred)
 
 # Random Forest Model
-rf_model <- randomForest(Survived ~.,data = train, method='class',ntree = 500, cutoff=c(.5,.5))
+rf_model <- randomForest(Survived ~.,data = train, method='class')
 rf_pred <- predict(rf_model,newdata = test,type = 'class')
 print("#Random Forest")
-print(table(test$Survived,rf_pred))
+perf_measures(test$Survived,rf_pred)
 
 # Support Vector Machines
 svm_model <- svm(Survived ~ .,data=train)
 svm_pred <- predict(svm_model,test)
 print("#Support Vector Machines")
-print(table(test$Survived,svm_pred))
+perf_measures(test$Survived,svm_pred)
