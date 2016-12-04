090bf884061f73af93c9b6ef00d36460e20fe20b
adi pradhan <adip@umich.edu>
2016-12-04 15:17:20 -0500

    add SVM to the models

--- diag-ml.R
+++ diag-ml.R
@@ -1,7 +1,7 @@
 # adi
 # 12/4/2016
 # packages for DIAG ML session
-
+set.seed(42)
 setwd("~/MBA/DIAG/ml-workshop")
 rm(list=ls())
 
@@ -20,6 +20,7 @@ library(rpart)
 library(rpart.plot)
 library(dplyr)
 library(ROCR)
+library(e1071)
 
 data <- read.csv('data/labelled_data.csv')
 unlabelled <- read.csv('data/unlabelled_data.csv')
@@ -39,26 +40,36 @@ train <- raw_train[comp,]
 comp <- complete.cases(raw_test)
 test <- raw_test[comp,]
 
+# Logistic Regression
+lm_model <- glm(Survived ~ .,data=train,family = 'binomial')
+p <- predict(lm_model, newdata=test, type="response")
+pr <- prediction(p, test$Survived)
+
+# Create a AUC curve
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+# AUC for the model
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+print("#Logistic Regression")
+print(auc)
+
 # Decision Tree Model
 rpart_model <- rpart(Survived ~ .,data=train, method='class')
 rpart.plot(rpart_model)
 rpart_pred <- predict(rpart_model,newdata = test,type = 'class')
+print("#Classification and Regression Trees (CART)")
 print(table(test$Survived,rpart_pred))
 
 # Random Forest Model
 rf_model <- randomForest(Survived ~.,data = train, method='class',ntree = 500, cutoff=c(.5,.5))
 rf_pred <- predict(rf_model,newdata = test,type = 'class')
+print("#Random Forest")
 print(table(test$Survived,rf_pred))
 
-
-# Logistic Regression
-lm_model <- glm(Survived ~ .,data=train,family = 'binomial')
-p <- predict(lm_model, newdata=test, type="response")
-pr <- prediction(p, test$Survived)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf)
-
-auc <- performance(pr, measure = "auc")
-auc <- auc@y.values[[1]]
-print(auc)
-
+# Support Vector Machines
+svm_model <- svm(Survived ~ .,data=train)
+svm_pred <- predict(svm_model,test)
+print("#Support Vector Machines")
+print(table(test$Survived,svm_pred))
